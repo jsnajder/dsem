36052e29470b49e291b3eeb3b4e2c683615b6633
Jan Snajder <jan.snajder@fer.hr>
2013-12-19 11:35:13 +0100

    bow pairs sim added

---
+++ src/bow-pairs-sim.hs
@@ -0,0 +1,35 @@
+{-------------------------------------------------------------------------------
+
+ Computes BOW similarity scores over a list of word pairs
+
+ (c) 2013 Jan Snajder <jan.snajder@fer.hr>
+
+-------------------------------------------------------------------------------}
+
+import qualified DSem.VectorSpaceModel.BowCached as Bow
+import qualified Data.Text as T
+import Text.Printf
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Trans
+import System.IO
+import System.Environment
+import System.Exit
+import Data.Maybe
+
+main = do
+  args <- getArgs
+  when (length args < 2) $ do
+    putStrLn "Usage: bow-pairs-sim <bow matrix> <list of word pairs>"
+    exitFailure
+  m  <- Bow.readMatrix (args!!0)
+  ps <- map (parse . reverse . words) . lines <$> readFile (args!!1)
+  Bow.runModelIO m $ do
+    Bow.setCacheSize 100
+    forM_ ps $ \(w1,w2) -> do
+      s <- fromMaybe (-1) <$> Bow.targetCosine (T.pack w1) (T.pack w2)
+      liftIO . putStrLn $ printf "%s\t%s\t%f" w1 w2 s
+      -- add: vector norms + overlapping dimensions
+  where parse (w2:w1:_) = (w1,w2)
+        parse _         = error "no parse"
+
