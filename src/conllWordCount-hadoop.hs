850a24b3ebafcd3a6d2ba967755aa0dbb4df90f9
Jan Snajder <jan.snajder@fer.hr>
2014-03-11 18:36:11 +0100

    conllWordCount-hadoop.hs added

---
+++ src/conllWordCount-hadoop.hs
@@ -0,0 +1,60 @@
+-- (c) 2013 Jan Snajder
+--
+-- Word counts mapper/reducer for Hadoop streams for CONLL parsed file
+-- Counts word forms
+--   -w  wordforms
+--   -l  lemmas
+--   -L  lemmas, with fallback to wordforms in case of '<unknown>'
+--   -p  concatenates pos tags
+
+import System.IO
+import System.Environment
+import Data.Word
+import Data.List.Split
+import qualified Data.Map.Strict as M
+import qualified Data.ByteString.UTF8 as B
+import ConllReader
+import qualified Counts as C
+import Control.Monad
+
+type WordCounts = C.Counts B.ByteString
+
+xMap :: (Token -> [String]) -> String -> String
+xMap f = unlines . rights . concatMap (parse . removeBlank) . lines
+  where parse [] = []
+        parse l  = maybe [] f (parseLine l)
+        removeBlank = unwords . words
+
+mrReduce :: IO ()
+mrReduce = do
+  xs <- getContents
+  putStr . showCounts . C.fromList . map B.fromString $ lines xs
+
+showCounts :: WordCounts -> String
+showCounts = 
+  unlines . map (\(w,c) -> B.toString w ++ "\t" ++ show c) . C.counts
+
+main :: IO ()
+main = do
+  args <- getArgs
+  hSetEncoding stdin utf8
+  hSetEncoding stdout utf8
+  if length args == 0 then 
+    putStrLn "Usage: conllWordCount-hadoop [-m -[w|l|L] [-p] | -r]"
+  else case (args!!0) of
+      "-m" -> 
+        let mrMap = case (args!!1) of
+                  "-w" -> if "-p" `elem` args then
+                            xMap (\t -> [form t ++ "_" ++ postag t])
+                          else xMap (\t -> [form t])
+                  "-L" -> if "-p" `elem` args then
+                            xMap (map showLP . lemmaPos')
+                          else xMap lemma'
+                  _    -> if "-p" `elem` args then
+                            xMap (map showLP . lemmaPos)  
+                          else xMap lemma
+        in interact mrMap
+      "-r" -> mrReduce
+      _    -> error "missing option -m or -r"
+
+
